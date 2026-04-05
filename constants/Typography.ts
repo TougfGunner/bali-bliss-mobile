ccd2d69663ac37e73ff91c88c1793ed34c532bee
TougfGunner <pereravik@gmail.com>
2026-04-05 13:53:55 +0100

    Add constants/Typography.ts

---
+++ constants/Typography.ts
@@ -0,0 +1,44 @@
+export const FontFamilies = {
+  display: 'PlayfairDisplay_700Bold',
+  displayItalic: 'PlayfairDisplay_700Bold_Italic',
+  displayRegular: 'PlayfairDisplay_400Regular',
+  sans: 'Montserrat_400Regular',
+  sansMedium: 'Montserrat_500Medium',
+  sansSemiBold: 'Montserrat_600SemiBold',
+  sansBold: 'Montserrat_700Bold',
+  raleway: 'Raleway_400Regular',
+  ralewayMedium: 'Raleway_500Medium',
+  ralewaySemiBold: 'Raleway_600SemiBold',
+  ralewayBold: 'Raleway_700Bold',
+  script: 'GreatVibes_400Regular',
+};
+
+export const FontSizes = {
+  xs: 11,
+  sm: 13,
+  base: 15,
+  md: 17,
+  lg: 20,
+  xl: 24,
+  '2xl': 30,
+  '3xl': 36,
+  '4xl': 44,
+  '5xl': 56,
+};
+
+export const LineHeights = {
+  tight: 1.2,
+  snug: 1.35,
+  normal: 1.5,
+  relaxed: 1.65,
+  loose: 2,
+};
+
+export const LetterSpacing = {
+  tight: -0.5,
+  normal: 0,
+  wide: 1,
+  wider: 2,
+  widest: 4,
+  ultraWide: 6,
+};
