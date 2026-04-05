9984d6a77733a1052b64f4369471ddb92356dcc4
TougfGunner <pereravik@gmail.com>
2026-04-05 13:53:53 +0100

    Add constants/Brand.ts

---
+++ constants/Brand.ts
@@ -0,0 +1,18 @@
+export const Brand = {
+  name: 'Bali Bliss',
+  tagline: 'The Ultimate Bali Wedding Guide',
+  fullName: 'Bali Bliss Weddings',
+  location: 'Bali, Indonesia',
+  email: 'hello@baliblissweddings.com',
+  phone: '+62 858 2154 6132',
+  instagram: 'https://www.instagram.com/BaliBlissWeddings',
+  facebook: 'https://www.facebook.com/BaliBlissWeddings',
+  pinterest: 'https://pinterest.com/baliblissweddings/',
+  website: 'https://baliblissweddings.com',
+  aboutText:
+    'Bali Bliss is a full-service Wedding Planner based in Bali. Your premier destination for inspiration, planning, and vendor connections.',
+  bannerText:
+    'Book your 2026 Bali destination wedding and enjoy complimentary planning services.',
+};
+
+export const API_BASE_URL = 'https://baliblissweddings.com/api';
